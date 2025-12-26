adbc53d4384a87e6c666c46a296510b21e8da30d
kasimkkn <kasimkkn15@gmail.com>
2025-12-26 17:49:31 +0530

    work on only db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,15 +18,14 @@ import { UserSeeder } from './database/seeders/user.seeder';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [User, Availability, Booking],
-        synchronize: true, // Set to false in production
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true, // dev only
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
       inject: [ConfigService],
     }),
